650c249fb2196a2515d5069859bfd59f005ffce0
Ramaa3 <ramnasyaifaarliansyah@student.telkomuniversity.ac.id>
2026-04-05 20:32:16 +0700

    menambahkan implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using TP_MOD6_103022400143;
 
-class Program
+namespace TP_MOD6_103022400143
 {
-    static void Main(string[] args)
+    class Program
     {
-        // Membuat object
-        SayaMusicTrack track1 = new SayaMusicTrack("Hati-Hati di Jalan");
+        static void Main(string[] args)
+        {
+            try
+            {
+                SayaMusicTrack track1 = new SayaMusicTrack("Lagu Santai");
 
-        // Menambah play count
-        track1.IncreasePlayCount(10);
-        track1.IncreasePlayCount(5);
+                track1.IncreasePlayCount(5000000);
+                track1.PrintTrackDetails();
 
-        // Menampilkan detail
-        track1.PrintTrackDetails();
+                track1.IncreasePlayCount(20000000); // kena assert
+
+                for (int i = 0; i < 100; i++)
+                {
+                    track1.IncreasePlayCount(int.MaxValue);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Terjadi error: " + e.Message);
+            }
+        }
     }
 }
--- SayaMusicTrack.cs
+++ SayaMusicTrack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,27 +9,38 @@ namespace TP_MOD6_103022400143
 
     class SayaMusicTrack
     {
-        // Atribut
+       
         private int id;
         private int playCount;
         private string title;
 
-        // Constructor
+
         public SayaMusicTrack(string title)
         {
+            Debug.Assert(title != null, "Title tidak boleh null");
+            Debug.Assert(title.Length <= 100, "Title maksimal 100 karakter");
+
             Random rand = new Random();
-            this.id = rand.Next(10000, 99999); // 5 digit random
+            this.id = rand.Next(10000, 99999);
             this.title = title;
             this.playCount = 0;
         }
-
-        // Method untuk menambah play count
         public void IncreasePlayCount(int count)
         {
-            playCount += count;
-        }
+            Debug.Assert(count <= 10000000, "Penambahan play count maksimal 10.000.000");
 
-        // Method untuk print detail
+            try
+            {
+                checked
+                {
+                    playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR: Terjadi overflow pada play count!");
+            }
+        }
         public void PrintTrackDetails()
         {
             Console.WriteLine("ID Track   : " + id);
