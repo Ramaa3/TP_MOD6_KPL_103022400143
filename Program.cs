740fe10d88d022d7ab10aa70227e27c95a45fafe
Ramaa3 <ramnasyaifaarliansyah@student.telkomuniversity.ac.id>
2026-04-05 20:08:04 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MOD6_103022400143;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat object
+        SayaMusicTrack track1 = new SayaMusicTrack("Hati-Hati di Jalan");
+
+        // Menambah play count
+        track1.IncreasePlayCount(10);
+        track1.IncreasePlayCount(5);
+
+        // Menampilkan detail
+        track1.PrintTrackDetails();
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MOD6_103022400143
+{
+    using System;
+
+    class SayaMusicTrack
+    {
+        // Atribut
+        private int id;
+        private int playCount;
+        private string title;
+
+        // Constructor
+        public SayaMusicTrack(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999); // 5 digit random
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        // Method untuk menambah play count
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        // Method untuk print detail
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID Track   : " + id);
+            Console.WriteLine("Title      : " + title);
+            Console.WriteLine("Play Count : " + playCount);
+            Console.WriteLine("---------------------------");
+        }
+    }
+
+}
